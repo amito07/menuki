37ba00394443f18007d0b566ec1b1aa89dc94cf8
Tahmid Akter <tahmidakter56@gmail.com>
2023-08-02 10:41:56 +0600

    variants added

--- app/restaurants/[id]/page.jsx
+++ app/restaurants/[id]/page.jsx
@@ -1,18 +1,33 @@
 'use client';
 import React, { useEffect, useState } from 'react';
+import { styled } from '@mui/material/styles';
 import { sliders } from '@/app/utils/fakeData';
 import { categories } from '@/app/utils/fakeCategory';
 import TextField from '@mui/material/TextField';
-import { Card, CardHeader, CardMedia, Grid } from '@mui/material';
+import { Card, CardHeader, CardMedia, Collapse, Grid } from '@mui/material';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { fakeFoods } from '@/app/utils/fakeFood';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import IconButton from '@mui/material/IconButton';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 const RestaurantPage = ({ params }) => {
     const [categoryList, setCategoryList] = useState([]);
     const [selectedCategory, setSelectedCategory] = useState('');
     const [foods, setFoods] = React.useState(fakeFoods);
+    const [expanded, setExpanded] = React.useState([]);
     const selectedCategoryStyle = {
         backgroundColor: 'orange',
         color: '#fff',
@@ -32,10 +47,17 @@ const RestaurantPage = ({ params }) => {
         const newCategories = new Set(categories);
         setCategoryList(Array.from(newCategories));
     };
+
+    const handleExpandClick = (index) => {
+        const newExpandedList = [...foods];
+        newExpandedList[index].expanded = !newExpandedList[index].expanded;
+        setExpanded(newExpandedList);
+    };
+
     useEffect(() => {
-        setFoods(fakeFoods);
+        fakeFoods.map((food) => (food.expanded = false));
         loadCategories(fakeFoods);
-    }, [foods]);
+    }, []);
     return (
         <div style={{ width: '100%' }}>
             <div style={{ marginTop: '80px', marginLeft: '0px' }}>
@@ -112,7 +134,7 @@ const RestaurantPage = ({ params }) => {
                                             {food.name ?? ''}
                                         </Typography>
                                         <Typography variant="body2" color="text.secondary">
-                                            Lizards are a widespread group
+                                            {food.desc ?? ''}
                                         </Typography>
                                         <div style={{ display: 'flex' }}>
                                             <Typography variant="body2" color="text.secondary">
@@ -121,8 +143,90 @@ const RestaurantPage = ({ params }) => {
                                             <Typography variant="body" color="text.primary" sx={{ marginLeft: '5px' }}>
                                                 {food.actual_price}
                                             </Typography>
+                                            <ExpandMore
+                                                expand={food.expanded}
+                                                onClick={(e) => handleExpandClick(index)}
+                                                aria-expanded={expanded}
+                                                aria-label="show more"
+                                            >
+                                                <ExpandMoreIcon />
+                                            </ExpandMore>
                                         </div>
                                     </CardContent>
+                                    <Collapse in={food.expanded} timeout="auto" unmountOnExit>
+                                        <CardContent>
+                                            <Typography paragraph>Variants:</Typography>
+                                            <hr />
+                                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                <Typography variant="h6">{food.variants[0].variant}</Typography>
+                                                <div style={{ display: 'flex', marginTop: '3px' }}>
+                                                    {food.discount_available == 1 ? (
+                                                        <Typography variant="body2" color="text.secondary">
+                                                            <del>{food.variants[0].actual_price}</del>
+                                                        </Typography>
+                                                    ) : (
+                                                        ''
+                                                    )}
+                                                    <Typography
+                                                        variant="body"
+                                                        color="text.primary"
+                                                        style={{ marginLeft: '5px' }}
+                                                    >
+                                                        {food.discount_available == 1
+                                                            ? food.variants[0].discounted_price
+                                                            : food.variants[0].actual_price}
+                                                    </Typography>
+                                                </div>
+                                            </div>
+                                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                <Typography variant="h6">{food.variants[1].variant}</Typography>
+                                                <div style={{ display: 'flex', marginTop: '3px' }}>
+                                                    {food.discount_available == 1 ? (
+                                                        <Typography variant="body2" color="text.secondary">
+                                                            <del>{food.variants[1].actual_price}</del>
+                                                        </Typography>
+                                                    ) : (
+                                                        ''
+                                                    )}
+                                                    <Typography
+                                                        variant="body"
+                                                        color="text.primary"
+                                                        style={{ marginLeft: '5px' }}
+                                                    >
+                                                        {food.discount_available == 1
+                                                            ? food.variants[1].discounted_price
+                                                            : food.variants[1].actual_price}
+                                                    </Typography>
+                                                </div>
+                                            </div>
+                                            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                                <Typography variant="h6">{food.variants[2].variant}</Typography>
+                                                <div style={{ display: 'flex', marginTop: '3px' }}>
+                                                    {food.discount_available == 1 ? (
+                                                        <Typography variant="body2" color="text.secondary">
+                                                            <del>{food.variants[2].actual_price}</del>
+                                                        </Typography>
+                                                    ) : (
+                                                        ''
+                                                    )}
+                                                    <Typography
+                                                        variant="body"
+                                                        color="text.primary"
+                                                        style={{ marginLeft: '5px' }}
+                                                    >
+                                                        {food.discount_available == 1
+                                                            ? food.variants[2].discounted_price
+                                                            : food.variants[2].actual_price}
+                                                    </Typography>
+                                                </div>
+                                            </div>
+                                            <hr />
+                                            <Typography paragraph>
+                                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                                aside for 10 minutes.
+                                            </Typography>
+                                        </CardContent>
+                                    </Collapse>
                                 </Card>
                             </Grid>
                         ))}
